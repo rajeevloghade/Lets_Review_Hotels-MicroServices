d2bf98bb5b6b45810e9143bf9a203b6f92d91822
rajeevloghade <rajeevloghade010@gmail.com>
2022-11-25 13:50:39 +0530

    registered all services on eureka server

--- hotel-service/src/main/java/com/microservice/hotel/HotelServiceApplication.java
+++ hotel-service/src/main/java/com/microservice/hotel/HotelServiceApplication.java
@@ -4,8 +4,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HotelServiceApplication {
 
 	private static final Logger LOGGER = LogManager.getLogger(HotelServiceApplication.class);
--- rating-service/src/main/java/com/microservice/rating/RatingServiceApplication.java
+++ rating-service/src/main/java/com/microservice/rating/RatingServiceApplication.java
@@ -4,8 +4,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingServiceApplication {
 
 	private static final Logger LOGGER = LogManager.getLogger(RatingServiceApplication.class);
